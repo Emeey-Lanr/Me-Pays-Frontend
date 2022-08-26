860777749dc557248069e4398fa87146791502e8
Oyelowo Emmanuel <emeeylanr04@gmail.com>
2022-08-26 03:50:46 +0100

    did the validation for the signup page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './component/Dashboard';
 import Erropage from './component/Erropage';
 import Indexpage from './component/Indexpage';
 import Signin from './component/Signin';
@@ -11,7 +12,7 @@ function App() {
         <Route path='/' element={<Indexpage />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/signin' element={<Signin />} />
-
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='*' element={<Erropage />} />
       </Routes>
 
--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -2,7 +2,65 @@ import './login.css'
 import Logo from './Logo'
 import { FaUserAlt } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 const Signup = () => {
+    const phoneregex = /^[\d]{11}$/
+    const mailregex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setlastName] = useState('')
+    const [email, setemail] = useState('')
+    const [phonenumber, setphonenumber] = useState('')
+    const [password, setpassword] = useState('')
+    const [passwordValidation, setpasswordValidation] = useState('')
+    const [phonevalidation, setPhoneValidation] = useState('')
+    const [mailvalidation, setmailvalidation] = useState('')
+    const [btndisable, setbtndisable] = useState(false)
+    const [emptyinput, setemptyinput] = useState('')
+    //emailvalidation
+    const yourmail = (e) => {
+        if (mailregex.test(e.target.value)) {
+            setbtndisable(false)
+            setemail(e.target.value)
+            setmailvalidation('')
+        } else {
+            setbtndisable(true)
+            setmailvalidation('Invalid email')
+        }
+    }
+    //Password Validation
+    const passw = (e) => {
+        if (e.target.value.trim().length < 6) {
+            setpasswordValidation('password must be a least 6 characters')
+            setbtndisable(true)
+        } else {
+            setpasswordValidation('')
+            setpassword(e.target.value)
+            setbtndisable(false)
+        }
+    }
+    //Phone number validation
+    const phone = (e) => {
+
+        if (phoneregex.test(e.target.value)) {
+            setPhoneValidation('')
+            setphonenumber(e.target.value)
+            setbtndisable(false)
+        } else {
+            setPhoneValidation('Invalid phone number')
+            setbtndisable(true)
+        }
+    }
+    //navigation 
+    let navigate = useNavigate()
+    const signupAcc = () => {
+        // navigate('/dashboard')
+        if (firstName === '' || lastName === '' || email === '' || phonenumber === '' || password === '') {
+            setemptyinput('Looks like you forgot something')
+        } else {
+            setemptyinput('')
+        }
+    }
     return (
         <>
             <div className="signupbody">
@@ -16,29 +74,38 @@ const Signup = () => {
                             <div className='inputsect'>
                                 <p>FirstName</p>
                                 <div>
-                                    <span><FaUserAlt /> </span>  <input type="text" />
+                                    <span><FaUserAlt /> </span>  <input type="text" onChange={(e) => setFirstName(e.target.value)} />
                                 </div>
                                 <p>Last Name</p>
                                 <div>
-                                    <span></span> <input type="text" />
+                                    <span></span> <input type="text" onChange={(e) => setlastName(e.target.value)} />
                                 </div>
                                 <p>Email</p>
                                 <div>
-                                    <span></span><input type="text" />
+                                    <span></span><input type="text" onChange={(e) => yourmail(e)} />
                                 </div>
+                                <p style={{ color: 'red', fontSize: "0.8rem" }}>{mailvalidation}</p>
                                 <p>Phone Number</p>
                                 <div>
-                                    <span></span> <input type="text" />
+                                    <span></span> <input type="text" onChange={(e) => phone(e)} />
                                 </div>
+                                <p style={{ color: 'red', fontSize: "0.8rem" }}>{phonevalidation}</p>
                                 <p>Password</p>
                                 <div>
-                                    <span></span> <input type="text" />
+                                    <span></span> <input type="text" onChange={(e) => passw(e)} />
                                 </div>
+                                <p className='passval' style={{ color: 'red', fontSize: "0.8rem" }}>{passwordValidation}</p>
                             </div>
+                            <p style={{ textAlign: "center", fontSize: '0.8rem', color: 'red' }}>{emptyinput}</p>
+                            <p className='gotan'>Got An Account Already</p>
                             <div className='rod'>
-                                <div className='rod1'></div>
-                                <Link to='/signin'>Signin</Link>
+                                <div className='rod1'>
+                                </div>
+                                <Link to='/signin' className='signinlink'>Signin</Link>
                                 <div className='rod2'></div>
+                            </div>
+                            <div className="btndiv">
+                                <button disabled={btndisable} onClick={() => signupAcc()}>Sign Up</button>
                             </div>
 
                         </div>
