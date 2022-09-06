2b0c95936a7cffcc355183d9d409cecb48fadc16
Oyelowo Emmanuel <emeeylanr04@gmail.com>
2022-09-06 10:46:28 +0100

    added some words to the index page

--- src/component/Indexpage.jsx
+++ src/component/Indexpage.jsx
@@ -1,5 +1,10 @@
 import Navbar from "./Navbar"
+import { useNavigate } from "react-router-dom"
 const Indexpage = () => {
+    const navigate = useNavigate()
+    const start = () => {
+        navigate('/signup')
+    }
     return (
         <>
             <Navbar />
@@ -9,10 +14,10 @@ const Indexpage = () => {
                         <span>ELECTRONIC PAYMENT SYSTEM</span></h2>
                     <div className="textcon">
                         <p><span className="textspan1">Fast</span> and  <span className="textspan2">Effective</span> way to start a plan for you future, transfer to your loved ones</p>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, omnis! lorem4 </p>
-                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Fast and efficient ways of getting that joy passed to people around you as you taste in it too.</p>
+                        <p>Me Pays does it better</p>
                         <div>
-                            <button>Get Started</button>
+                            <button onClick={() => start()}>Get Started</button>
                         </div>
 
                     </div>
